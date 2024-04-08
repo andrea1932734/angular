cbcd640542abbe91bd29a7be5647cc342ac821cb
andrea1932734 <andreacarovelli@gmail.com>
2024-04-08 07:46:38 +0000

    primo

---
+++ es_9/src/app/app.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { ListComponent } from './list/list.component';
+import { NavigationComponent } from './navigation/navigation.component';
+import { IntroductionComponent } from './introduction/introduction.component';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet,ListComponent,NavigationComponent,IntroductionComponent],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = 'es_9';
+}
